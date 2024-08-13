f8abf7b2ef1359484e1347341b516596b8d99593
egboka <egbokasamuel1999@gmail.com>
2024-08-13 08:18:40 +0100

    Add changes to the footer,header and maincontent file

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,9 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
-import Header from './components/Header Component/Header';
-import MainContent from './components/main content/MainContent';
-import Footer from './components/Footer Component/Footer';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 function App() {
     const [count, setCount] = useState(0)
 
