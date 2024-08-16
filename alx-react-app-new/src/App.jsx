c2b725d7a0d0f6a97600424c7bfb7ef5ed8c7fe8
egboka <egbokasamuel1999@gmail.com>
2024-08-16 19:34:24 +0100

    Initial commit: Added Counter application with useState hook

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,13 +1,15 @@
 import WelcomeMessage from "./components/WelcomeMessage";
-import Header from "./Header";
-import MainContent from "./MainContent";
-import Footer from "./Footer";
-import UserProfile from "./UserProfile";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
+import Counter from "./Counter";
 
 function App() {
   return (
     <>
       {/* <WelcomeMessage /> */}
+      <Counter />
       <Header />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
