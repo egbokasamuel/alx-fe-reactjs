5908b2a79dd9284583dd8a15360e4a515e5ad87e
egboka <egbokasamuel1999@gmail.com>
2024-08-17 17:43:45 +0100

    Refactored application to use Context API instead of prop drilling

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,28 @@
+// import WelcomeMessage from "./components/WelcomeMessage";
+// import Header from "./Header";
+// import MainContent from "./MainContent";
+// import Footer from "./Footer";
+// import UserProfile from "./UserProfile";
+import ProfilePage from "./ProfilePage";
+import UserContext from "./UserContext";
+// import UserInfo from "./UserInfo";
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  return (
+    <>
+      {/* return <ProfilePage userData={userData} />;
+      <UserInfo /> */}
+      <UserContext.Provider value={userData}>
+        <ProfilePage />
+      </UserContext.Provider>
+      {/* <WelcomeMessage /> */}
+      {/* <Header /> */}
+      {/* <MainContent /> */}
+      {/* <UserProfile name="Alice" age="25" bio="Loves hiking and photography" /> */}
+      {/* <Footer /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ alx-react-app-props/src/UserContext.js
@@ -0,0 +1,8 @@
+import { createContext } from "react";
+
+const UserContext = createContext({
+  name: "",
+  email: "",
+});
+
+export default UserContext;
