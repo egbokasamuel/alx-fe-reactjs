431e973978e2069c32df46498eeed08897ceec8f
egboka <egbokasamuel1999@gmail.com>
2024-08-26 07:29:03 +0100

    modified reciptlist

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,26 +1,22 @@
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
+import SearchBar from "./components/SearchBar";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
+      <SearchBar />
       <Router>
         <Switch>
           <Route path="/" exact>
             <RecipeList />
             <AddRecipeForm />
           </Route>
-          <Route
-            path="/recipe/:id"
-            render={(props) => (
-              <RecipeDetails recipeId={parseInt(props.match.params.id)} />
-            )}
-          />
+          <Route path="/recipe/:id" component={RecipeDetails} />
         </Switch>
       </Router>
     </div>
