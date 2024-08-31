1b8d9eaf04a2188c98840d7e97ac9efba314e565
egboka <egbokasamuel1999@gmail.com>
2024-08-31 13:52:25 +0100

    Add some changes to protectedroute.jsx file

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth"; // Import the useAuth hook
 
-function ProtectedRoute({ isAuthenticated, children }) {
-  return isAuthenticated ? children : <Navigate to="/login" />;
-}
+const ProtectedRoute = ({ children }) => {
+  const isAuthenticated = useAuth(); // Use the useAuth hook
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />; // Redirect to login if not authenticated
+  }
+
+  return children; // Render the protected component if authenticated
+};
 
 export default ProtectedRoute;
